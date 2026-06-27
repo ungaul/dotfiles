d0279f1e187413f6398dd87dd7e536134ddab479
ungaul <github@0x404.org>
2026-06-27 18:50:10 +0200

    sync: 2026-06-27T18:50:10+02:00

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
-fastfetch
-set -g fish_greeting ""
+  fastfetch
+  set -g fish_greeting ""
 end
+
 set -gx TERM xterm-256color
 # nnn earthy green/brown theme
 # nnn using Alacritty's normal green
@@ -11,11 +11,15 @@ set -gx NNN_COLORS '#02020303;2222'
 # block char dir exe regular hardlink symlink missing orphan fifo socket other
 set -gx NNN_FCOLORS '000002020003080808080300'
 set -gx NNN_OPTS 'eH'
+
 if test -d /run/user/1000/hypr
     set -gx HYPRLAND_INSTANCE_SIGNATURE (ls -t /run/user/1000/hypr/ | head -1)
 end
-set -gx WAYLAND_DISPLAY (ls /run/user/1000/ | string match -r 'wayland-[0-9]+' | head -1)
-set -gx XDG_SESSION_TYPE wayland
+if test -n "$WAYLAND_DISPLAY"
+    set -gx XDG_SESSION_TYPE wayland
+else
+    set -gx XDG_SESSION_TYPE x11
+end
 set -gx XDG_RUNTIME_DIR /run/user/1000
 
 # bun
